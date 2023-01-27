a2ec9b135073f874f332d6a7a2807fd30cbe8938
OlirahetaX <oliveriraheta57@gmail.com>
2023-01-27 14:02:19 -0600

    Primer commit

---
+++ src/lab2p2/Lab2P2.java
@@ -0,0 +1,21 @@
+package lab2p2;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Lab2P2 {
+
+    public static void main(String[] args) {
+        ArrayList<Usuarios> usu = new ArrayList();
+        int opc = 0;
+        while (opc != 4) {
+            System.out.println("""
+                               1- Registro de Inmueble/Solar
+                               2- Manejo de Estados
+                               3- Log In/Sign Up
+                               4- Salir""");
+            
+        }
+    }
+
+}
