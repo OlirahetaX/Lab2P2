09aa58098ef9130297764ef66a921a6756cf70a4
OlirahetaX <oliveriraheta57@gmail.com>
2023-01-27 14:20:47 -0600

    2 commit; Clase usuarios creada

--- src/lab2p2/Lab2P2.java
+++ src/lab2p2/Lab2P2.java
@@ -1,12 +1,15 @@
 package lab2p2;
 
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+
 
 public class Lab2P2 {
 
     public static void main(String[] args) {
+        ArrayList<Bienes>bienes = new ArrayList();
         ArrayList<Usuarios> usu = new ArrayList();
+        usu.add(new Usuarios("ADMIN","admin","admin1234",15));
+        
         int opc = 0;
         while (opc != 4) {
             System.out.println("""
@@ -14,7 +17,11 @@ public class Lab2P2 {
                                2- Manejo de Estados
                                3- Log In/Sign Up
                                4- Salir""");
-            
+            if (usu.size()!=0) {
+                
+            }else{
+                System.out.println(">>> CREE UN USUARIO <<<");
+            }
         }
     }
 
